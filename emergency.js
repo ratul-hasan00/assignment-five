c17cf25bb122d4ec484909f3f4ad997bc583994b
ratul-hasan00 <afif.ratul00@gmail.com>
2025-08-29 17:37:54 +0600

    added all js script code

--- emergency.js
+++ emergency.js
@@ -1,3 +1,50 @@
+// Call history btn here 
+
+let callsForHistory = document.getElementsByClassName("call-btn") ;
+let historyClear = document.getElementById("history-clear") ;
+let historyLists = document.getElementById("history-lists") ;
+
+let coinForHistory = document.getElementById("coin-count") ;
+
+for (const history of callsForHistory){
+   history.addEventListener("click", function(){
+
+      let coinTextForHistory = coinForHistory.innerText ;
+      let CoinToNumberForHistory = parseInt(coinTextForHistory) ;
+
+      if(CoinToNumberForHistory < 20 ) {
+         return ;
+      }
+
+      let cardForHistory = history.closest(".card") ; 
+      let nameForHistory = cardForHistory.querySelector(".name").innerText ;
+      let numberForHistory = cardForHistory.querySelector(".number").innerText ;
+      let timeForHistory = new Date().toLocaleTimeString() ;
+
+      let createHistory = document.createElement("div") ;
+      createHistory.classList.add("flex", "justify-between", "items-center", "bg-[#fafafa]", "rounded-lg", "w-67", "h-20", "mx-auto", "mt-2", "px-2") ;
+
+      createHistory.innerHTML =  `
+      <div>
+         <p class = "font-bold">${nameForHistory}</p>
+         <p class="text-[#5c5c5c]">${numberForHistory}</p>
+      </div>
+
+      <div>
+         <p>${timeForHistory}</p>
+      </div>
+
+      `;
+      historyLists.appendChild(createHistory) ; 
+      
+   })
+}
+
+historyClear.addEventListener("click", function () {
+    historyLists.innerHTML = "" ;
+  });
+
+
 // by touching heart icon the heart count will increase here  
 
 const heartCount = document.getElementById("heart-count");
@@ -75,4 +122,5 @@ for (const copy of copyBtn){
    )
 }
 
-         
+
+
